b6a95e7f268264af11bb9343698ac323dbc0b9ed
Eduardo Soliz <eduardozend@gmail.com>
2015-04-28 18:09:13 -0400

    add sqlite and storage

--- app/src/main/java/com/memoryapp/sozo/NewUser.java
+++ app/src/main/java/com/memoryapp/sozo/NewUser.java
@@ -3,8 +3,10 @@ package com.memoryapp.sozo;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
+import android.database.Cursor;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -12,10 +14,13 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import android.database.sqlite.SQLiteDatabase;
+
+import java.sql.ResultSet;
 
 
 public class NewUser extends ActionBarActivity {
-
+    public UsuariosSQLiteHelper usdbh;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +32,13 @@ public class NewUser extends ActionBarActivity {
         //return home view
         Button btnReturnHome = (Button) findViewById(R.id.button3);
         btnReturnHome.setOnClickListener(BotonListenerReturnHome);
+
+        //storage
+        SharedPreferences newUser = getSharedPreferences("newUserData",0);
+        Log.d("storage values", "valor de la variable guardada  "+newUser.getString("newUser",""));
+        usdbh = new UsuariosSQLiteHelper(this, "DBUsuarios", null, 3);
+
+
 
     }
     private OnClickListener BotonListenerReturnHome = new OnClickListener(){
@@ -49,11 +61,32 @@ public class NewUser extends ActionBarActivity {
                 Toast.makeText(view.getContext(),R.string.passwordError,Toast.LENGTH_SHORT).show();
             }else {
 
-                SharedPreferences newUser = getSharedPreferences("newUserData",MODE_PRIVATE);
-                Editor editNewUser = newUser.edit();
+                SharedPreferences newUser = getSharedPreferences("newUserData", 0);
+                SharedPreferences.Editor editNewUser = newUser.edit();
                 editNewUser.putString("newUser",et.getText().toString());
                 editNewUser.putString("newPassword",et2.getText().toString());
                 editNewUser.commit();
+
+                //sqLite
+                SQLiteDatabase db = usdbh.getWritableDatabase();
+                db.execSQL("INSERT INTO user (name,password,language,presentation,sound,time) VALUES ('"+et.getText().toString()+"', '"+et2.getText().toString()+"','spanish','directo','1','45')");
+
+
+                SQLiteDatabase mydatabase = openOrCreateDatabase("DBUsuarios",MODE_PRIVATE,null);
+                Cursor resultSet = mydatabase.rawQuery("Select * from user",null);
+                resultSet.moveToFirst();
+                String username = resultSet.getString(1);
+
+                while (resultSet.moveToNext()) {
+                    Log.d("sqlite", "valor id  "+resultSet.getString(0));
+                    Log.d("sqlite", "valor name  "+resultSet.getString(1));
+                    Log.d("sqlite", "valor password  "+resultSet.getString(2));
+                }
+                mydatabase.close();
+
+
+
+                //end sqlite
 
                 Intent i = new Intent(view.getContext(), Configuration.class);
                 startActivity(i);
